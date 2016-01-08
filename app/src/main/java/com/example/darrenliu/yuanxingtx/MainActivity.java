b0995aa93f936705dd2014c58ddd9d46ce317611
Liu Yujie <1041195457@qq.com>
2016-01-08 13:31:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/darrenliu/yuanxingtx/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.darrenliu.yuanxingtx;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int CAMERA_REQUEST_CODE = 1;
+    private static int GALLERY_REQUEST_CODE = 2;
+    private static int CROP_REQUEST_CODE = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_camera = (Button)findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+
+        Button btn_gallery = (Button)findViewById(R.id.btn_gallery);
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_REQUEST_CODE);
+            }
+        });
+    }
+
+    private Uri saveBitmap(Bitmap bm)
+    {
+        File tmpDir = new File(Environment.getExternalStorageDirectory() + "/com.touxiang.avater");
+        if(!tmpDir.exists())
+        {
+            tmpDir.mkdir();
+        }
+        File img = new File(tmpDir.getAbsolutePath() + "avater.png");
+        try {
+            FileOutputStream fos = new FileOutputStream(img);
+            bm.compress(Bitmap.CompressFormat.PNG, 85, fos);
+            fos.flush();
+            fos.close();
+            return Uri.fromFile(img);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Uri convertUri(Uri uri)
+    {
+        InputStream is= null ;
+        try {
+            is = getContentResolver().openInputStream(uri);
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+            return saveBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void startImageZoom(Uri uri)
+    {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CROP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CAMERA_REQUEST_CODE)
+        {
+            if(data == null)
+            {
+                return;
+            }
+            else
+            {
+                Bundle extras = data.getExtras();
+                if(extras != null)
+                {
+                    Bitmap bm = extras.getParcelable("data");
+                    Uri uri = saveBitmap(bm);
+                    startImageZoom(uri);
+                }
+            }
+        }
+        else if(requestCode == GALLERY_REQUEST_CODE)
+        {
+            if(data == null)
+            {
+                return;
+            }
+            Uri uri;
+            uri = data.getData();
+            Uri fileUri = convertUri(uri);
+            startImageZoom(fileUri);
+        }
+        else if(requestCode == CROP_REQUEST_CODE)
+        {
+            if(data == null)
+            {
+                return;
+            }
+            Bundle extras = data.getExtras();
+            if(extras == null){
+                return;
+            }
+            Bitmap bm = extras.getParcelable("data");
+            ImageView imageView = (ImageView)findViewById(R.id.imageView);
+            imageView.setImageBitmap(bm);
+        }
+    }
+}
